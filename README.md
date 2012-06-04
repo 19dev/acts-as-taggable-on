aa430289928d56a123a043bd47c86097f0c0a5fa
seyyah <nurettin.senyer@bil.omu.edu.tr>
2012-06-04 09:43:51 +0300

    model:user

--- README.md
+++ README.md
@@ -2,10 +2,15 @@ acts-as-taggable-on
 
 Komutlar,
 
-$ rails generate acts_as_taggable_on:migration
-$ rake db:migrate
+	$ rails generate acts_as_taggable_on:migration
+	$ rake db:migrate
 
+User model,
 
+	$ rails g model User name:string
+	$ rake db:migrate
+
+İlişkisellik, ![ilişki](http://yuml.me/fd245769.jpg)
 
 # Kaynak
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120604063523) do
+ActiveRecord::Schema.define(:version => 20120604063814) do
 
   create_table "taggings", :force => true do |t|
     t.integer  "tag_id"
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(:version => 20120604063523) do
 
   create_table "tags", :force => true do |t|
     t.string "name"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
